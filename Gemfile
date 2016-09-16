811d0b016566cbba03575d09faf0ee9ad9ca6e35
Sungwon Choe <dokkaebi@gmail.com>
2016-09-16 23:51:04 +0000

    added factory girl

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
+  gem "factory_girl_rails", "~> 4.0"
 end
 
 gem 'simple_form'
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -17,11 +17,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       get :new
@@ -36,11 +32,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       post :create, gram: {message: 'Hello!'}
@@ -52,11 +44,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       gram_count = Gram.count
